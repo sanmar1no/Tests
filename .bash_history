e3aa822b3accec4bc548080f8a09b690a08f51b8
sanmar1no <sanmar1no@mail.ru>
2020-09-24 19:20:39 +0300

    First commit

---
+++ .bash_history
@@ -0,0 +1,47 @@
+md .git
+cmd
+git status
+git remote add origin https://github.com/sanmar1no/NewProjectTests.git
+git init
+git remote add origin https://github.com/sanmar1no/NewProjectTests.git
+git status
+git add .
+git commit -m "First commit"
+git config --global user.name "sanmar1no"
+git config --global user.email sanmar1no@mail.ru
+git add .
+git commit -m "First commit"
+git remote -v
+git push -u origin master
+git branch -M master
+git push -u origin master
+git remote
+git push -u origin master
+git status
+git commit -m "First commit"
+git add .
+git commit -m "First commit"
+git status
+git commit -m "2 commit"
+git add .
+git commit -m "2 commit"
+git status
+git branch -M master
+git add .
+git commit -m "2 commit"
+git push -u origin master
+$ git config --global user.name "sanmar1no"
+git config --global user.name "sanmar1no" 
+git config --global user.email sanmar1no@mail.ru
+git init
+git status
+git add .
+git commit -m "First commit"
+git remote add origin https://github.com/sanmar1no/NewProjectTests.git
+git remote
+git branch -M master
+git push -u origin master
+git status
+git pull
+git status
+git push -u origin master
---
+++ Tests/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tests
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //public readonly static int test = 10;
+        //public const int test_const = 20;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // test = 11;//присваивание значение доступному только для чтения статическому полю допускается только с статическом конструкторе или в инициализаторе переменных
+           // test_const = 21;//левая часть выражения должна быть переменной, свойством или индексатором
+            variableKey key = new variableKey();
+            
+            values Eleven = new values(12);            
+            label1.Text = Eleven.GetValue(key.KeyInteger());
+            button1.Text = Eleven.GetValue(key.KeyString());
+        }
+        
+    }
+
+}
+class variableKey
+{
+    string keyS = "string";
+    string keyI = "int";
+    public string KeyString()
+    {
+        return this.keyS.ToString();
+    }
+    public string KeyInteger()
+    {
+        return this.keyI.ToString();
+    }
+}
+class values //класс
+{
+    public int test = 10;
+    public string test_string = "20";
+    
+
+    public string GetValue(string key)
+    {
+        if (key == "int") return this.test.ToString();
+        if (key == "string") return this.test_string;
+        return "";
+    }
+    public values(int twelvty)// коструктор 1
+    {
+        this.test = twelvty;
+    }
+}
